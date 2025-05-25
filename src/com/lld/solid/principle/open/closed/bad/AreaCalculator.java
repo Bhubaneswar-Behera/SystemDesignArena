a88aefdd408a2b942898b1aa8453edcbf8604499
Self <bhubaneswar.behera@gmail.com>
2025-05-25 14:35:18 +0530

    SRP and OCP

--- src/com/lld/solid/principle/open/closed/bad/AreaCalculator.java
+++ src/com/lld/solid/principle/open/closed/bad/AreaCalculator.java
@@ -2,8 +2,12 @@ package com.lld.solid.principle.open.closed.bad;
 
 public class AreaCalculator {
     int calculateArea(Shape shape) {
-        if (shape.type.equals("circle")) { return 1; } // fake values
-        else if (shape.type.equals("rectangle")) { return 2; }
+        if (shape.type.equals("circle")) {
+            return 1;
+        }
+        else if (shape.type.equals("rectangle")) {
+            return 2;
+        }
         return 0;
     }
 }
